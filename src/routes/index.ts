fd0b267f7df4af9c78a0ecfda4cfd36bf4651a8d
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-17 20:03:20 +0700

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import UserController from '../controllers/user'
 
 const router = Router()
 
 router.get('/', (req, res) => res.send({ message: 'Server test ok' }))
+
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
 
 router.get('/*', (req, res, next) =>
   next({ status: 404, message: 'URI does not exist. Please make sure the uri is typed correctly.' })
