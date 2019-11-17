8d2668eeec26c758a10d9e3c46b60390226fe15f
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-18 00:49:50 +0700

    up

--- src/bin/https.ts
+++ src/bin/https.ts
@@ -1,16 +1,15 @@
 import app from '../app'
 import https from 'https'
-// import fs from 'fs'
+import fs from 'fs'
 
 const domain = process.env.DOMAIN
 const PORT = 443
 
-// const options = {
-//   cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
-//   key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`)
-// }
+const options = {
+  cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
+  key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
+}
 
-// const server = https.createServer(options, app)
-const server = https.createServer(app)
+const server = https.createServer(options, app)
 
 server.listen(PORT, () => console.log('HTTPS Server running on port', PORT))
--- vmdeploy.sh
+++ vmdeploy.sh
@@ -2,4 +2,6 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
 sudo apt-get install -y nodejs
 npm install
 npm install -g pm2
+sudo apt-get install certbot
+sudo certbot certonly --standalone
 npm run deploy
